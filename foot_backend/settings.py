1c2c78bdf4e94952fa9efbfaf43f2def79024869
unknown <C:\Users\\AppData\Roaming\The Bat!>
2021-03-17 20:43:57 +0700

    Diploy

--- foot_backend/settings.py
+++ foot_backend/settings.py
@@ -82,11 +82,23 @@ WSGI_APPLICATION = 'foot_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '1324dcv1@!12',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
+
+    },
 }
 DATABASES['default'].update(db_from_env)
 
