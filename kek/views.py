65062caf2a14ff1525799c5578936f8055c07e6b
unknown <C:\Users\\AppData\Roaming\The Bat!>
2021-03-17 21:41:03 +0700

    Diploy

--- kek/views.py
+++ kek/views.py
@@ -32,7 +32,7 @@ class Registr(APIView):
         serializer = reg_seria(data=request.query_params)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
-            return Response({'Greate user regist':'True'})
+            return Response({'Greate user regist':True})
         else:
            return Response({"Error":"CHECK"})
 # class Auth(APIView):
